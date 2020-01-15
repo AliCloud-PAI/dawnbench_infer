60f63128d2f6a9e7b3aa26d24b47eabe5318afff
root <root@h53a03319.sqa.eu95>
2020-01-15 12:28:21 +0800

    initial commit

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@
 sudo docker run -ti --name dawn_eval --net=host --volume-driver=nvidia-docker \
                                     --volume nvidia_driver_418.67:/usr/local/nvidia:ro \
                                     --device /dev/nvidia0:/dev/nvidia0 \
-                                    --device /dev/nvidia1:/dev/nvidia1 \
                                     --device /dev/nvidiactl:/dev/nvidiactl \
                                     --device /dev/nvidia-uvm:/dev/nvidia-uvm \
                                     --privileged=true -v /home/dawnbench_infer:/app \
